2933cda34c36e957a522c27b927cc58ec49eac2d
Lluvia <friasllu@gmail.com>
2023-03-30 11:22:44 -0400

    commit tree 0fa2d86781942ab2aba48c5b61d2fd4f635e4c28

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :name
+
+    def initialize(word)
+      @name = word
+    end
+
+    def match(possible_anagram)
+       possible_anagram.select {|x| x.split("").sort == @name.split("").sort}
+       #".select" method will select words that match given attributes in the block passed
+       #x here stands for word, we split the word into seperate string/letters and sort them 
+       #if they are equivalent to the instance variable word (that's also split into separate strings/letters and sort)
+       #then it will give us any anagrams that may be found in our possible_anagram array argument passed to this match method.
+    end
+end
